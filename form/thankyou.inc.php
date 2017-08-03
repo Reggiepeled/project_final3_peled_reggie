8c0a2f3f1b4e097b6de458550f4e51914f4e2887
reggie peled <revital.peled@gmail.com>
2017-08-03 18:48:03 -0400

    add form functionality

---
+++ form/thankyou.inc.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>The Prancing Pony</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="img/favicon.png" type="image/png" />
+
+    <link rel="stylesheet" href="css/vendor/normalize.min.css">
+    <link rel="stylesheet" href="css/hamburger.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <h1>The Prancing Pony</h1>
+        <div class="logo">
+            <img src="img/logo.png" alt="Logo" title="Logo" />
+        </div>
+        <!-- hamburger menu buttons ------>
+        <button class="hamburger">&#9776;</button>
+        <button class="cross">&#735;</button>
+    </header>
+
+    <!-- navigation -->
+    <nav class="menu">  <!-- hamurger menu -->
+        <ul>
+            <a href="index.html"><li>Home</li></a>
+            <a href="menu.html"><li>Menu</li></a>
+            <a href="contact.html"><li>Contact</li></a>
+            <a href="Locations.html"><li>Locations</li></a>
+            <a href="events.html"><li>Events and Promotions</li></a>
+        </ul>
+    </nav>
+
+    <nav class="desktop">   <!-- desktop menu -->
+        <ul>
+            <a href="index.html"><li>Home</li></a>
+            <a href="menu.html"><li>Menu</li></a>
+            <a href="contact.html"><li>Contact</li></a>
+            <a href="Locations.html"><li>Locations</li></a>
+            <a href="events.html"><li>Events and Promotions</li></a>
+        </ul>
+    </nav>
+    <div class="container">
+        <main>
+            <h2>Thank you for your Reservation</h2>
+            <p>Will are looking forward to have you here.</p>
+        </main>
+    </div>
+    <footer>
+        <section class="foot">
+            <h5>Call Us:</h5>
+            <p>1 - 800 - 770 - 6600</p>
+        </section>
+        <section class="foot">
+            <h5>We are always open (almost)</h5>
+            <p>Mon - Friday: 7:00 am - 2:00 am</p>
+            <p>Sat - Sun: 9:00 am - 5:00 am</p>
+        </section>
+        <section class="foot social">
+            <h5>Follow Us:</h5>
+            <ul>
+                <li><a href="https://www.facebook.com/revital.peled.10" target="_blank" title="Facebook">
+                    <img src="img/facebook.png" alt="Facebook" title="Facebook" /></a></li>
+                <li><a href="https://twitter.com/ReggiePeled" target="_blank" title="Twitter">
+                    <img src="img/twitter.png" alt="Twitter" title="Twitter" /></a></li>
+                <li><a href="https://www.youtube.com/watch?v=r1j9LVVUiFg" target="_blank" title="YouTube">
+                    <img src="img/youtube.png" alt="YouTube" title="YouTube" /></a></li>
+            </ul>
+        </section>
+    </footer>
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/humburgermenu.js"></script>
+    <script src="js/jquery.form.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>

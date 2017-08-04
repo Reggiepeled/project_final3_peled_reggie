9b905bb70c7b5111c295dfdfef5e04fda04e5eae
reggie peled <revital.peled@gmail.com>
2017-08-04 09:06:07 -0400

    add functionality to the reserve button
    when clicking the button, go to contact page

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@ $(document).ready(function () {
     $('#date').pickadate();
     //for the time picker plugin
     $('#hour').pickatime();
+
+    //go to contact page when clicking the reservation button
+    $('.reserve').click(function () {
+        window.location.href = 'contact.html';
+    });
 });
